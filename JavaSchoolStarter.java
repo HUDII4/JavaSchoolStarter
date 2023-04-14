ffb0295d3ed2ec5c5a5cb091f37f33715e551eb9
HUDII4 <107413849+HUDII4@users.noreply.github.com>
2023-04-14 18:08:05 +0300

    Update JavaSchoolStarter.java

--- JavaSchoolStarter.java
+++ JavaSchoolStarter.java
@@ -1,66 +1,106 @@
-package com.digdes.school;
-
+package com.diget.school;
 
 import java.util.*;
-import java.util.stream.Stream;
 
 public class JavaSchoolStarter {
-   public JavaSchoolStarter(){
 
-   }
+    public JavaSchoolStarter() {
+    }
 
-  public List<Map<String, Object>> execute(String request)throws Exeption{
-       Map<String, Object> row = new HashMap<>();
-        List<Map<String, Object>> data = new ArrayList<>();
-        Set<Map.Entry<String, Object>> entryRow = row.entrySet();
-        if (req.startsWith("INSERT VALUES")){
+    public List<Map<String, Object>> execute(String req) throws Exception {
+        List<Map<String, Object>> userLine = new ArrayList<>();
+        Map<String, Object> userRow = new HashMap<>();
+        int where = req.indexOf("Where");
+        int from = req.indexOf("From");
+        if (req.startsWith("INSERT VALUES")) {
             req = req.substring(14, req.length());
-            for (String pair : req.split(", ")){
-                String[] kv = pair.split("=");
-                Object obj = kv[1];
-                row.put(kv[0], obj);
+            try {
+                for (String pair : req.split(", ")) {
+                    String[] keyValue = pair.split("=");
+                    Object obj = keyValue[1];
+                    userRow.put(keyValue[0], obj);
+                }
+                userLine.add(userRow);
+            } catch (NullPointerException e) {
+                throw new RuntimeException();
             }
-            data.add(row);
-       } else if (request.startsWith("UPDATE VALUES")) {
-           int index = request.indexOf("Where");
-           request = request.substring(14, index);
-           if (data.contains(row.containsKey("'active'")) && data.contains(row.containsValue(("true")))){
-           for (String pair1 : request.split(" ")) {
-               String[] keyValue = pair1.split("=");
-                Object obj = keyValue[1];
-                  row.replace(keyValue[0], obj);
-               }
-               }
-       }else if (request.startsWith("DELETE")) {
-           int index = request.indexOf("Where");
-           request = request.substring(7, index);
-           if (data.contains(row.containsKey("'active'")) && data.contains(row.containsValue(("true")))){
-           for (String pair1 : request.split(", ")) {
-               String[] keyValue = pair1.split("=");
-              Object obj = keyValue[1];
-                  row.remove(keyValue[0], obj);
-               }
-               }
-       }
-     return data;
-  }
+        } else if (req.startsWith("UPDATE VALUES")) {
+            String res = req.substring(where - 1, req.length());
+            req = req.substring(14, where);
+            try{
+                    for (Map.Entry<String, Object> entry : userRow.entrySet()){
+                        if (entry.getKey().equals("'cost'") && entry.getValue().equals(12.5)){
+                            for (String pair : req.split(", ")) {
+                                String[] keyValue = pair.split("=");
+                                Object obj = keyValue[1];
+                                userRow = (Map<String, Object>) entry;
+                                userRow.replace(keyValue[0], obj);
+                        }
+                    } else {
+                            System.err.println(":)");
+                        }
+                }
+                userLine.add(userRow);
+            } catch (NullPointerException e) {
+                throw new RuntimeException();
+            }
+        } else if (req.startsWith("DELETE")){
+                req = req.substring(where, req.length());
+                try{
+                 for (Map.Entry<String, Object> entry : userRow.entrySet()){
+                     for (String str : req.split(" ")){
+                         String[] keyValue = str.split("=");
+                         Object obj = keyValue[1];
+                         if (entry.getKey().equals(keyValue[0]) && entry.getValue().equals(obj)) {
+                             userRow = (Map<String, Object>) entry;
+                             userRow.remove(keyValue[0], obj);
+                         }
+                     }
+                 }
+            } catch (NullPointerException e) {
+                throw new RuntimeException();
+            }
+        } else if (req.startsWith("SELECT")){
+            req = req.substring(where, req.length());
+            try{
+                for (Map.Entry<String, Object> entry : userRow.entrySet()){
+                    for (String str : req.split(" ")){
+                        String[] keyValue = str.split("=");
+                        Object obj = keyValue[1];
+                        if (entry.getKey().equals(keyValue[0]) && entry.getValue().equals(obj)) {
+                            userRow = (Map<String, Object>) entry;
+                            userRow.getOrDefault(keyValue[0], obj);
+                        }
+                    }
+                }
+            } catch (NullPointerException e) {
+                throw new RuntimeException();
+            }
+        }
+        return userLine;
+    }
 }
 
-class Gag {
-   public static void main(String[] args) {
-  try {
-         JavaSchoolStarter start = new JavaSchoolStarter();
+class Test{
+    public static void main(String[] args) {
+        try {
+            Database dt = new Database();
+            List<Map<String, Object>> user0 = dt.execute("INSERT VALUES 'ID'= 3, 'lastname' ='hello', 'age'=10, 'cost' = 56.5, 'active'=true ");
+            List<Map<String, Object>> user1 = dt.execute("INSERT VALUES 'ID'= 12, 'lastname' ='Ttitov', 'age'=12, 'cost' = 12.5, 'active'=true ");
+            List<Map<String, Object>> user2 = dt.execute("UPDATE VALUES 'ID'= 13, 'lastname' ='Ttitov', Where 'cost' = 12.5 ");
+            List<Map<String, Object>> user3 = dt.execute("UPDATE VALUES 'ID'= 13, 'lastname' ='Ttitov', Where 'cost' like 12.5 ");
+            List<Map<String, Object>> user4 = dt.execute("DELETE Where 'ID' = 3 ");
+            List<Map<String, Object>> user5 = dt.execute("SELECT Where 'ID' = 12 ");
 
-         List<Map<String, Object>> map1 = start.execute("INSERT VALUES 'id' = 7, 'lastname' = 'Pasentsyan', 'age' = 18, 'cost' = 2.18, 'active' = false");
-       List<Map<String, Object>> map2 = start.execute("INSERT VALUES 'id' = 7, 'lastname' = 'Ttov', 'age' = 18, 'cost' = 21.18, 'active' = true");
-       List<Map<String, Object>> map3 = start.execute("INSERT VALUES 'id' = 72, 'lastname' = 'Карпов', 'age' = 27, 'cost' = 5.5, 'active' = true");
-       List<Map<String, Object>> map4 = start.execute("DELETE 'id'=33, 'age'=34 Where 'active'=true ");
-       List<Map<String, Object>> map5 = start.execute("UPDATE VALUES 'id'=33, 'age'=34 Where 'active'=true ");
-       System.out.println(map1);
-       System.out.println(map2);
-       System.out.println(map3);
-       System.out.println(map4);
-       System.out.println(map4);
-  }catch(Exeption w) throw new RuntimeExeption(e);
-   }
+            System.out.println(user0);
+            System.out.println(user1);
+            System.out.println(user2);
+            System.out.println(user3);
+            System.out.println(user4);
+            System.out.println(user5);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
